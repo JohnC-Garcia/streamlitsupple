0ce65c7a81bb749e09300979d9b2d36b20cc14bf
JohnCarlosGarcia <148402930+JohnC-Garcia@users.noreply.github.com>
2025-04-21 23:41:22 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,27 +1,34 @@
 import streamlit as st
 import tensorflow as tf
-
-@st.cache(allow_output_mutation=True)   
-model = load_model('weather_classifier.h5')
-st.write("""# Weather Detection System""")
-file=st.file_uploader("Choose weather photo from computer",type=["jpg","png"])
-
+from tensorflow.keras.models import load_model
 import cv2
-from PIL import Image,ImageOps
+from PIL import Image, ImageOps
 import numpy as np
-def import_and_predict(image_data,model):
-    size=(64,64)
-    image=ImageOps.fit(image_data,size,Image.LANCZOS)
-    img=np.asarray(image)
-    img_reshape=img[np.newaxis,...]
-    prediction=model.predict(img_reshape)
+
+@st.cache(allow_output_mutation=True)
+def load_weather_model():
+    return load_model('weather_classifier.h5')
+
+model = load_weather_model()
+
+st.write("""# Weather Detection System""")
+file = st.file_uploader("Choose weather photo from computer", type=["jpg", "png"])
+
+def import_and_predict(image_data, model):
+    size = (64, 64)
+    image = ImageOps.fit(image_data, size, Image.LANCZOS)
+    image = image.convert("RGB")  # make sure it's in RGB
+    img = np.asarray(image)
+    img_reshape = img[np.newaxis, ...] / 255.0  # normalize
+    prediction = model.predict(img_reshape)
     return prediction
+
 if file is None:
     st.text("Please upload an image file")
 else:
-    image=Image.open(file)
-    st.image(image,use_column_width=True)
-    prediction=import_and_predict(image,model)
+    image = Image.open(file)
+    st.image(image, use_column_width=True)
+    prediction = import_and_predict(image, model)
     class_names = ['cloudy', 'rain', 'shine', 'sunrise']
-    string="OUTPUT : "+class_names[np.argmax(prediction)]
+    string = "OUTPUT : " + class_names[np.argmax(prediction)]
     st.success(string)
